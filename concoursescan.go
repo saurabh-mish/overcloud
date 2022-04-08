cbf1465f5d103020e45f55e6f10426a23d5f1915
Saurabh Mishra <saurabh.mish@icloud.com>
2022-04-08 14:51:41 -0700

    retrieve environment variables for CONCOURSE_USERNAME and CONCOURSE_PASSWORD

--- concoursescan.go
+++ concoursescan.go
@@ -13,14 +13,14 @@ func checkCredentials() (*string, *string) {
 
 	concourseUser, present = os.LookupEnv("CONCOURSE_USERNAME")
 	if concourseUser == "" || !present  {
-		fmt.Println("Environment variable 'CONCOURSE_USERNAME' empty or not set ...")
+		fmt.Fprint(os.Stdout, "Environment variable 'CONCOURSE_USERNAME' empty or not set ...")
 	} else {
 		flag.StringVar(&concourseUser, "username", concourseUser, "Username (Email) for Concourse Labs")	
 	}
 	
 	concoursePass, present = os.LookupEnv("CONCOURSE_PASSWORD")
 	if concoursePass == "" || !present {
-		fmt.Println("Environment variable 'CONCOURSE_PASSWORD' empty or not set ...")
+		fmt.Fprint(os.Stdout, "Environment variable 'CONCOURSE_PASSWORD' empty or not set ...")
 	} else {
 		flag.StringVar(&concoursePass, "password", concoursePass, "Password for Concourse Labs")	
 	}
